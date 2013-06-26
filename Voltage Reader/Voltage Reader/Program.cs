9f1c950469a30123a665cc4967a3563ff8e5033b
Joshua312 <joshcheung98@gmail.com>
2013-06-26 14:27:12 +0100

    NetDuino Application 10 - Voltage Reader (3 ERRORS as API is Not Level)

---
+++ Voltage Reader/Voltage Reader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Voltage_Reader
+{
+    public class VoltageReader
+    {
+        public static void Main()
+        {
+
+            const double maxVoltage = 3.3;
+            const int maxAdcValue = 1023;
+
+            var voltagePort = new AnalogInput(Pins.GPIO_PIN_A1);
+            var lowPort = new OutputPort(Pins.GPIO_PIN_A0, false);
+            var highPort = new OutputPort(Pins.GPIO_PIN_A2, true);
+
+            while (true)
+            {
+                int rawValue = voltagePort.Read();
+                double value = (rawValue * maxVoltage) / maxAdcValue;
+                Debug.Print(rawValue + " " + value.ToString("f"));
+            }
+        }
+
+    }
+}
