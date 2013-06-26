09060e1fd2c539c8616c42908a363297fa0d88bd
Joshua312 <joshcheung98@gmail.com>
2013-06-26 14:01:03 +0100

    NetDuino Application 8 - LED Flicker

---
+++ NetduinoApplication8/NetduinoApplication8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace NetduinoApplication8
+{
+    public class BlinkingLed
+    {
+        public static void Main()
+        {
+            var ledPort = new OutputPort(Pins.ONBOARD_LED, false);
+
+            while (true)
+            {
+                ledPort.Write(true);
+                Thread.Sleep(500);
+
+                ledPort.Write(false);
+                Thread.Sleep(500);
+            }
+
+
+        }
+
+    }
+}
