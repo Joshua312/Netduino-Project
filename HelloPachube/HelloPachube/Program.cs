b4f480afb50a006ca2bb606703b89f3ef35ee710
Joshua312 <joshcheung98@gmail.com>
2013-06-27 13:57:50 +0100

    NetDuino Project - HelloPachube

---
+++ HelloPachube/HelloPachube/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace HelloPachube
+{
+    using System;
+    using System.Threading;
+
+    using Endjin.XivleyClient;
+
+    using Microsoft.SPOT;
+    using Microsoft.SPOT.Hardware;
+    using SecretLabs.NETMF.Hardware.Netduino;
+
+    public class HelloPachube
+    {
+        public static void Main()
+        {
+            const string ApiKey = "FHowwfdHmgYTCvyfYMilfzIN52OwdkGc1ZmcUPfyfKjbMElQ";
+            const string FeedId = "620930332";
+            const int SamplingPeriod = 20000;
+
+            const double MaxVoltage = 3.3;
+            const int MaxAdcValue = 1023;
+
+            var voltagePort = new AnalogInput(new Cpu.AnalogChannel());
+
+            try
+            {
+                var lowPort = new OutputPort(Pins.GPIO_PIN_A0, false);
+                var highPort = new OutputPort(Pins.GPIO_PIN_A2, true);
+            }
+            catch (Exception exception)
+            {
+                Debug.Print(exception.ToString());    
+            }
+
+            while (true)
+            {
+                WaitUntilNextPeriod(SamplingPeriod);
+                double rawValue = voltagePort.Read();
+                double value = (rawValue * MaxVoltage) / MaxAdcValue;
+                string sample = "{ \"voltage\":\"" + value.ToString("f") + "\"}";
+                Debug.Print("new message:  " + sample);
+                XivleyClient.Send("FHowwfdHmgYTCvyfYMilfzIN52OwdkGc1ZmcUPfyfKjbMElQ", "620930332", sample);
+            }
+        }
+
+        private static void WaitUntilNextPeriod(int period)
+        {
+            long now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            var offset = (int)(now % period);
+            int delay = period - offset;
+            Debug.Print("sleep for " + delay + "ms\r\n");
+            Thread.Sleep(delay);
+        }
+    }
+}
---
+++ HelloPachube/HelloPachube/XivleyClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Microsoft.SPOT;
+
+namespace Endjin.XivleyClient
+{
+    public static class XivleyClient
+    {
+        const string baseUri = "http://api.xively.com/v2/feeds/";
+
+        public static void Send(string apiKey, string feedId, string sample)
+        {
+            Debug.Print("time: " + DateTime.Now);
+            Debug.Print("memory available: " + Debug.GC(true));
+
+            try
+            {
+                using (var request = CreateRequest(apiKey, feedId, sample))
+                {
+                    request.Timeout = 5000;     // 5 seconds
+                    // send request and receive response
+                    using (var response = (HttpWebResponse)request.GetResponse())
+                    {
+                        HandleResponse(response);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.Message);
+            }
+        }
+
+        static HttpWebRequest CreateRequest(string apiKey, string feedId, string sample)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(sample);
+
+            var request = (HttpWebRequest)WebRequest.Create(baseUri + feedId);
+
+            // request line
+            request.Method = "PUT";
+
+            // request headers
+            request.ContentLength = buffer.Length;
+            request.ContentType = "application/json";
+            request.Headers.Add("X-ApiKey", apiKey);
+
+            // request body
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(buffer, 0, buffer.Length);
+            }
+
+            return request;
+        }
+
+        public static void HandleResponse(HttpWebResponse response)
+        {
+            Debug.Print("Status code: " + response.StatusCode);
+        }
+    }
+}
